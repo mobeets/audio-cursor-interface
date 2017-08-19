334c15ddb488d4528ae48dac56f54a37f228e709
mobeets <mobeets@gmail.com>
2017-08-19 10:15:42 -0400

    cleanup

--- sketch.js
+++ sketch.js
@@ -1,13 +1,17 @@
-var mic, fft, pos, vel, gain, A, B, c, trgPos;
+var mic, fft, pos, vel, A, B, c, trgPos;
+var gain = 0.001;
 var canvasWidth = 1200;
 var canvasHeight = 600;
-var nInputDims = 1024; // spectrum.length
 var circRadius = 25;
 var trgRadius = 50;
 var trgDistance = 200;
 var score = 0;
 var ntrials = 0;
 var cursorTrace;
+var spectrumSmoothing = 0.8; // between 0.0 and 1.0
+var spectrumBins = 1024; // 2^k for k between 4 and 10
+var userInput;
+var nInputDims = 1024; // spectrum.length
 
 function setup() {
    createCanvas(canvasWidth, canvasHeight);
@@ -16,54 +20,18 @@ function setup() {
    // monitor microphone input
    mic = new p5.AudioIn();
    mic.start();
-   fft = new p5.FFT();
+   fft = new p5.FFT(spectrumSmoothing, spectrumBins);
    fft.setInput(mic);
 
-   // initialize cursor position
-   pos = createVector(canvasWidth/2.0,canvasHeight/2.0);
-   trgPos = setRandomTarget();
-   vel = createVector(0,0);
-   gain = 0.001;
-   cursorTrace = new Array();
-   setDecoder(); // initializer decoder
+   // initialize experiment
+   startNewExperiment();
 }
 
-function draw() {
-   background(200);
-   drawTarget();
-
-   // show spectrum of mic input
-   var spectrum = fft.analyze();
-   noFill();
-   stroke(0);
-   beginShape();
-   for (i = 0; i<spectrum.length; i++) {
-    vertex(i, map(spectrum[i], 0, 255, height, 0) );
-   }
-   endShape();
-
-   // draw cursor trace
-   beginShape();
-   noFill();
-   stroke(0);
-   for (i = 0; i<cursorTrace.length; i++) {
-      vertex(cursorTrace[i].x, cursorTrace[i].y);
-   }
-   endShape();
-
-   // update decoder
-   vel = updateCursorVel(spectrum, vel, A, B, c);
-   pos = updateCursorPos(pos, vel, gain);
-   resetIfHitBoundary();
-
-   // update cursor
-   var clr = color(121, 141, 224);
-   fill(clr);
-   noStroke();
-   ellipse(pos.x, pos.y, circRadius); // x, y, w, h
-   checkIfCursorAcquiredTarget();
-   showScore();
-
+function startNewExperiment() {
+   setDecoder();
+   startNewTrial(false);
+   score = 0;
+   ntrials = 0;
 }
 
 function setDecoder() {
@@ -75,7 +43,74 @@ function setDecoder() {
    }
 }
 
-function drawTarget() {
+function startNewTrial(isSuccess) {
+   pos = createVector(canvasWidth/2.0,canvasHeight/2.0);
+   vel = createVector(0,0);
+   trgPos = setRandomTarget();
+   cursorTrace = new Array();
+   if (isSuccess) {
+      score = score + 1;
+   }
+   ntrials = ntrials + 1;
+}
+
+function draw() {
+
+   // draw target, cursor history, and score
+   background(200);
+   showTarget();
+   showCursorHistory();
+   showScore();
+
+   // display processed mic input
+   getAndShowInput();
+
+   // update decoder with mic input
+   updateAndDrawCursor();
+   checkIfCursorAcquiredTarget();   
+
+}
+
+function getAndShowInput() {
+   var spectrum = fft.analyze();
+   var amps = fft.logAverages(fft.getOctaveBands());
+   noFill();
+   stroke(0);
+   beginShape();
+   for (i = 0; i<spectrum.length; i++) {
+    vertex(i, map(spectrum[i], 0, 255, height, 0) );
+   }
+   // for (i = 0; i<amps.length; i++) {      
+   //    var vx = map(i, 0, amps.length, 0, width);
+   //    vertex(vx, map(amps[i], 0, 255, height, 0) );
+   // }
+   endShape();
+   userInput = spectrum;
+}
+
+function showCursorHistory() {
+   beginShape();
+   noFill();
+   stroke(0);
+   for (i = 0; i<cursorTrace.length; i++) {
+      vertex(cursorTrace[i].x, cursorTrace[i].y);
+   }
+   endShape();
+}
+
+function updateAndDrawCursor() {
+   vel = updateCursorVel(userInput, vel, A, B, c);
+   pos = updateCursorPos(pos, vel, gain);
+   resetIfHitBoundary();
+
+   // draw cursor
+   var clr = color(121, 141, 224);
+   fill(clr);
+   noStroke();
+   ellipse(pos.x, pos.y, circRadius); // x, y, w, h
+}
+
+function showTarget() {
    var clr = color(50, 54, 200);
    fill(clr);
    noStroke();
@@ -116,23 +151,8 @@ function updateCursorPos(pos, vel, gain) {
 
 function resetIfHitBoundary() {
    if (pos.x < 0 || pos.x > canvasWidth || pos.y < 0 || pos.y > canvasHeight) {
-      restartTrial();
+      startNewTrial(false);
    }
-}
-
-function mouseClicked() {
-   setDecoder();
-   restartTrial();
-   score = 0;
-   // ntrials = 0;
-}
-
-function restartTrial() {
-   pos.set(canvasWidth/2.0,canvasHeight/2.0);
-   vel.set(0,0);
-   trgPos = setRandomTarget();
-   ntrials = ntrials + 1;
-   cursorTrace = new Array();
 }
 
 function checkIfCursorAcquiredTarget() {
@@ -144,7 +164,7 @@ function checkIfCursorAcquiredTarget() {
       ellipse(trgPos.x, trgPos.y, trgRadius);
       score = score + 1;
       setTimeout("test", 5000);
-      restartTrial();
+      startNewTrial(true);
    }
 }
 
@@ -157,10 +177,12 @@ function showScore() {
   text("Press right arrow key to abort trial.\nClick mouse to change the decoder.", canvasWidth-300, 15);
 }
 
+function mouseClicked() {
+   startNewExperiment();
+}
+
 function keyPressed() {
    if (keyCode === RIGHT_ARROW) {
-      // setDecoder();
-      restartTrial();
-      // score = 0;
+      startNewTrial(false);
    }
 }
